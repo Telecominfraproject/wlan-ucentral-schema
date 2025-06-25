Telecominfraproject/wlan-ucentral-schema

commit d8260f8ff326ab062f11103714ba3b6f972f5014

add property that allows disabling MPSK
Signed-off-by: John Crispin <john@phrozen.org>

renderer/renderer.uc
@@ -733,6 +733,8 @@ let services = {
 			if (!interface.ssids)
 				continue;
 			for (let ssid in interface.ssids) {
+				if (!ssid?.enhanced-mpsk)
+					continue;
 				if ((ssid?.encryption?.proto && type(ssid.encryption.proto) == 'string' &&
 				    ssid.encryption.proto == "mpsk-radius") ||
 				    (type(ssid.multi_psk) == 'bool' && ssid.multi_psk))

schema/interface.ssid.yml
@@ -137,6 +137,11 @@ properties:
     maximum: 20
   encryption:
     $ref: "https://ucentral.io/schema/v1/interface/ssid/encryption/"
+  enhanced-mpsk:
+    description:
+      Optinally disable MPSK
+    type: boolean
+    default: true
   multi-psk:
     anyOf:
       - type: array

schemareader.uc
@@ -5857,6 +5857,20 @@ function instantiateInterfaceSsid(location, value, errors) {
 			obj.encryption = instantiateInterfaceSsidEncryption(location + "/encryption", value["encryption"], errors);
 		}
 
+		function parseEnhancedMpsk(location, value, errors) {
+			if (type(value) != "bool")
+				push(errors, [ location, "must be of type boolean" ]);
+
+			return value;
+		}
+
+		if (exists(value, "enhanced-mpsk")) {
+			obj.enhanced_mpsk = parseEnhancedMpsk(location + "/enhanced-mpsk", value["enhanced-mpsk"], errors);
+		}
+		else {
+			obj.enhanced_mpsk = true;
+		}
+
 		function parseMultiPsk(location, value, errors) {
 			function parseVariant0(location, value, errors) {
 				if (type(value) == "array") {

commit 7e22aa0aae43fdbf9ee26c9385946409c3c2b9c4

Fix typo in enhanced MPSK property name
Corrected property key from `enhanced-mpsk` to `enhanced_mpsk`

Fixes: d8260f8 ("add property that allows disabling MPSK")
Fixes: WIFI-14484

Signed-off-by: Marek Kwaczynski <marek@shasta.cloud>

renderer/renderer.uc
@@ -735,7 +735,7 @@ let services = {
 			if (!interface.ssids)
 				continue;
 			for (let ssid in interface.ssids) {
-				if (!ssid?.enhanced-mpsk)
+				if (!ssid?.enhanced_mpsk)
 					continue;
 				if ((ssid?.encryption?.proto && type(ssid.encryption.proto) == 'string' &&
 				    ssid.encryption.proto == "mpsk-radius") ||